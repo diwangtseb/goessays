cc4e4b3063c66ed99649cc19cde37d9260093b35
diwangtseb <diwangtseb@outlook.com>
2022-06-01 17:38:05 +0800

    update sync pool

--- pool/main.go
+++ pool/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"sync"
+)
 
+type Stock struct {
+	Symbol string
+	Price  int
+}
+
+func (s *Stock) String() string {
+	return fmt.Sprintf("%s: %d", s.Symbol, s.Price)
+}
+
+var stockPool = sync.Pool{
+	New: func() interface{} {
+		return &Stock{}
+	},
+}
+
+func main() {
+	var stocks []*Stock
+	for i := 0; i < 10; i++ {
+		stock := stockPool.Get().(*Stock)
+		stock.Symbol = "GOOG"
+		stock.Price = i
+		stockPool.Put(stock)
+		stocks = append(stocks, stock)
+	}
+	fmt.Printf("%v", stocks)
 }
