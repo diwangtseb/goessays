d54fc49f239ee387829ee35c1fa25c020738cbdc
diwang <diwang83963911@gamil.com>
2022-07-27 19:24:12 +0800

    update cron

--- workcorn/foobar/main.go
+++ workcorn/foobar/main.go
@@ -4,10 +4,26 @@ import (
 	"fmt"
 	hello "helloworld"
 	"time"
+
+	"github.com/robfig/cron/v3"
 )
 
 func main() {
 	hello.Hello()
 	fmt.Println("end")
+	now := time.Now().Local()
+	fmt.Println(timeToCronFromat(now))
+	c := cron.New(cron.WithSeconds())
+	c.AddFunc(timeToCronFromat(now), func() { fmt.Println("regularly perform") })
+	c.Start()
 	time.Sleep(time.Second * 10)
+}
+
+func timeToCronFromat(t time.Time) string {
+	second := t.Second()
+	minute := t.Minute()
+	hour := t.Hour()
+	day := t.Day()
+	formatCronStr := fmt.Sprintf("%d %d %d %d * *", second, minute, hour, day)
+	return formatCronStr
 }
--- workcorn/helloworld/hello.go
+++ workcorn/helloworld/hello.go
@@ -8,7 +8,7 @@ import (
 
 func Hello() {
 	c := cron.New()
-	c.AddFunc("* * * * *", func() { fmt.Println("every minutes") })
+	c.AddFunc("*/20 * * * *", func() { fmt.Println("every minutes") })
 	c.Start()
 	// c.Stop() // Stop the scheduler (does not stop any jobs already running).
 	fmt.Println("Hello, world!")
