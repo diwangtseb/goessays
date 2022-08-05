29f4e5eebe6fcc5ef1e260ab853121b7b1250fac
diwang <diwang83963911@gamil.com>
2022-08-05 16:05:40 +0800

    add nsq

---
+++ nsq/consumer/go.mod
@@ -0,0 +1,7 @@
+module consumer
+
+go 1.18
+
+require github.com/nsqio/go-nsq v1.1.0
+
+require github.com/golang/snappy v0.0.1 // indirect
---
+++ nsq/producer/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nsqio/go-nsq"
+)
+
+func main() {
+	// Instantiate a producer.
+	config := nsq.NewConfig()
+	producer, err := nsq.NewProducer("127.0.0.1:4150", config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	messageBody := []byte("hello")
+	topicName := "topic"
+
+	// Synchronously publish a single message to the specified topic.
+	// Messages can also be sent asynchronously and/or in batches.
+	err = producer.Publish(topicName, messageBody)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Gracefully stop the producer when appropriate (e.g. before shutting down the service)
+	producer.Stop()
+}
