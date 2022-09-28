4ff5e5fa80113f2e709a93cc406233ec10221bf8
diwang <diwang83963911@gamil.com>
2022-09-28 17:21:45 +0800

    update channel

---
+++ rbqdm/go.mod
@@ -0,0 +1,5 @@
+module rdqdm
+
+go 1.18
+
+require github.com/rabbitmq/amqp091-go v1.5.0
---
+++ rbqdm/receive/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/xx")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			handleMsg(d, handleMsgFunc(func(delivery amqp.Delivery) error {
+				log.Println(string(delivery.Body))
+				return nil
+			}))
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+type handleMsgFunc func(delivery amqp.Delivery) error
+
+func (h handleMsgFunc) handle(delivery amqp.Delivery) {
+	log.Println("start")
+	h(delivery)
+	log.Println("end")
+}
+
+func handleMsg(d amqp.Delivery, hmf ...handleMsgFunc) {
+	for _, h := range hmf {
+		h.handle(d)
+	}
+}
