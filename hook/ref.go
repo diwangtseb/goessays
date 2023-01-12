c14c28dcab8e19939498535796c8005b474be8b4
diwang <diwang83963911@gamil.com>
2023-01-12 16:32:58 +0800

    add case

---
+++ hook/ref.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+func main() {
+	Engine(&Person{})
+	Engine(&Order{})
+
+	// Output
+	// *Person.BeforeSave
+	// Save person data
+	// *Person.AfterSave
+	// Save order data
+}
+
+func Engine(valPtr interface{}) error {
+	if v, ok := valPtr.(interface{ BeforeSave() }); ok {
+		v.BeforeSave()
+	}
+	if v, ok := valPtr.(interface{ Save() }); ok {
+		v.Save()
+	}
+	if v, ok := valPtr.(interface{ AfterSave() }); ok {
+		v.AfterSave()
+	}
+	return nil
+}
+
+type Person struct {
+	FirstName string
+	LastName  string
+}
+
+func (p *Person) Save() {
+	// call beforeSave()
+	fmt.Println("Save person data")
+	// call afterSave()
+}
+func (p *Person) BeforeSave() { fmt.Println("*Person.BeforeSave") }
+func (p *Person) AfterSave()  { fmt.Println("*Person.AfterSave") }
+
+type Order struct {
+	Number ObjectId
+	Items  []Item
+}
+
+func (o *Order) Save() {
+	// call beforeSave()
+	fmt.Println("Save order data")
+	// call afterSave()
+}
+
+type Item struct{}     // for demonstration purpose
+type ObjectId struct{} // for demonstration purpose
