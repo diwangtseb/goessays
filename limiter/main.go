54bb2b0f77612feccc1e981f01d4c65250257c7a
diwang <diwang83963911@gamil.com>
2023-04-04 19:05:38 +0800

    update limiter

--- limiter/main.go
+++ limiter/main.go
@@ -2,33 +2,34 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
 	"golang.org/x/time/rate"
 )
 
-const bucket_size = 10
+const bucket_size = 100
+
+var l = rate.NewLimiter(1, bucket_size)
 
 func main() {
-	l := rate.NewLimiter(1, bucket_size)
-	for i := 0; i < 20; i++ {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
-		defer cancel()
-		go mockReq(ctx, l)
+	for i := 0; i < 120; i++ {
+		// ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+		// defer cancel()
+		go mockReq(context.TODO())
 		time.Sleep(time.Millisecond * 100)
 	}
 	select {}
 }
 
-func mockReq(ctx context.Context, rl *rate.Limiter) {
-	err := rl.Wait(ctx)
-	if err != nil {
-		fmt.Println("token is not ok", rl.Tokens(), err.Error())
-		return
-	}
+func mockReq(ctx context.Context) {
+	l.Wait(ctx)
+	// if err != nil {
+	// fmt.Println("token is not ok", rl.Tokens(), err.Error())
+	// return
+	// }
 	// rr := rl.Reserve()
 	// dur := rr.Delay()
-	log.Println("token is ok", rl.Tokens())
+	// time.Sleep(dur)
+	log.Println("token is ok", l.Tokens())
 }
