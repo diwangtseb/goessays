e4ecbd1f73eea025ad52a4325ed3cebc2d1a3ea1
diwang <diwang83963911@gamil.com>
2023-06-20 18:12:31 +0800

    add some dp case

--- algo/main.go
+++ algo/main.go
@@ -7,8 +7,11 @@ import (
 func main() {
 	r := minCoin([]int{3, 3, 3}, 10)
 	fmt.Println(r)
+	r = maxSubArray([]int{1, -2, 4, 3, -1, 0})
+	fmt.Println(r)
 }
 
+// min coins equal target coin in pockets
 func minCoin(pockets []int, targetCoin int) int {
 	dp := make([]int, targetCoin+1)
 	for i := 1; i <= targetCoin; i++ {
@@ -32,4 +35,28 @@ func min(a, b int) int {
 		return a
 	}
 	return b
+}
+
+// max continuous sum sub array
+func maxSubArray(nums []int) int {
+	n := len(nums)
+	if n == 0 {
+		return 0
+	}
+	dp := make([]int, n)
+	dp[0] = nums[0]
+	for i := 1; i < n; i++ {
+		if dp[i-1] > 0 {
+			dp[i] = dp[i-1] + nums[i]
+		} else {
+			dp[i] = nums[i]
+		}
+	}
+	ans := dp[0]
+	for i := 1; i < n; i++ {
+		if dp[i] > ans {
+			ans = dp[i]
+		}
+	}
+	return ans
 }
