1e732221ef55a1d73b40577a8f78bc031c0be538
diwang <diwang83963911@gamil.com>
2023-06-28 13:18:11 +0800

    enhance: add any cluster code

--- cluster/main.go
+++ cluster/main.go
@@ -2,22 +2,40 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
-	m1 := NewMachine("1", Leader)
-	m2 := NewMachine("2", Slave)
-	m3 := NewMachine("3", Slave)
+	m1 := NewMachine("192.168.0.1", Leader, 3)
+	m2 := NewMachine("192.168.0.2", Slave, 2)
+	m3 := NewMachine("192.168.0.3", Leader, 1)
 	cluster := NewCluster(m1, m2, m3)
 	cluster.use(func(m []*Machine) {
 		vote(m)
-		loadbalance(m)
 	})
-	for _, v := range cluster.machines {
-		fmt.Printf("%v", v)
+	// make a break
+	go func() {
+		time.Sleep(time.Second * 3)
+		fmt.Println("re vote !!!!")
+		m1.status = DISABLE
+		cluster.use(func(m []*Machine) {
+			vote(m)
+		})
+	}()
+	for i := 0; i < 100; i++ {
+		go cluster.trafficRequest(struct{}{})
+		if i > 50 {
+			time.Sleep(time.Second * 1)
+		}
 	}
+
+	// define os signal
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGTERM, os.Interrupt)
+	<-ch
 }
 
 type EnumRole string
@@ -27,19 +45,28 @@ const (
 	Slave  EnumRole = "slave"
 )
 
+type EnumStatus int
+
+const (
+	ABLE    EnumStatus = 0
+	DISABLE EnumStatus = -1
+)
+
 type Machine struct {
-	ip      string
-	role    EnumRole
-	payload int
-	weight  float64
+	ip            string
+	role          EnumRole
+	payload       int
+	weight        float64
+	currentWeight float64
+	status        EnumStatus
 }
 
-func NewMachine(ip string, role EnumRole) *Machine {
+func NewMachine(ip string, role EnumRole, weight float64) *Machine {
 	return &Machine{
-		ip:      ip,
-		role:    role,
-		payload: 0,
-		weight:  0,
+		ip:     ip,
+		role:   role,
+		weight: weight,
+		status: ABLE,
 	}
 }
 
@@ -66,29 +93,52 @@ func (c *Cluster) use(mf ...machineFunc) {
 }
 
 func vote(machines []*Machine) {
-	machines[0].setRole(Leader)
-	maxPlayloadMachine := machines[0]
-	for i := 1; i < len(machines); i++ {
-		if machines[i].payload <= maxPlayloadMachine.payload {
+	var maxPlayloadMachine *Machine
+	var disbaleCount int
+	for i := 0; i < len(machines); i++ {
+		if maxPlayloadMachine == nil {
+			if machines[i].status == DISABLE {
+				disbaleCount++
+				continue
+			}
+			machines[i].setRole(Leader)
+			maxPlayloadMachine = machines[i]
 			continue
 		}
-		maxPlayloadMachine.setRole(Slave)
-		machines[i].setRole(Leader)
-		maxPlayloadMachine = machines[i]
+		if machines[i].payload > maxPlayloadMachine.payload && machines[i].status == ABLE {
+			maxPlayloadMachine.setRole(Slave)
+			maxPlayloadMachine = machines[i]
+			maxPlayloadMachine.setRole(Leader)
+		} else {
+			machines[i].setRole(Slave)
+		}
+	}
+	if disbaleCount == len(machines) {
+		panic("all machines breakdown")
 	}
 }
 
-func loadbalance(machines []*Machine) {
-	total := 1.0
-	for idx, v := range machines {
-		v.weight = total - 0.25
-		if idx == len(machines) {
-			v.weight = total - v.weight
+func (c *Cluster) getSelectedMachine() *Machine {
+	var totalWeight float64
+	var selectedMachine *Machine
+	for i := 0; i < len(c.machines); i++ {
+		if c.machines[i].status == DISABLE {
+			continue
+		}
+		totalWeight += c.machines[i].weight
+		c.machines[i].currentWeight += c.machines[i].weight
+		if selectedMachine == nil || c.machines[i].currentWeight > selectedMachine.currentWeight {
+			selectedMachine = c.machines[i]
 		}
 	}
+	selectedMachine.currentWeight -= totalWeight
+	return selectedMachine
 }
 
 func (c *Cluster) trafficRequest(req interface{}) {
-	r := rand.New(rand.NewSource(time.Now().UnixMicro()))
-	r.Float64()
+	machine := c.getSelectedMachine()
+	defer func() {
+		fmt.Printf("current machine ip %s payload %d currentweight %f \n", machine.ip, machine.payload, machine.currentWeight)
+	}()
+	machine.payload++
 }
